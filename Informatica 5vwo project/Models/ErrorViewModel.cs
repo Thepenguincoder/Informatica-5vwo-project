d88c97e69f77c2ca3b8abd26b7f92c1bd75c74a2
110394 <110394@st-maartenscollege.nl>
2021-02-03 09:51:44 +0100

    Add project files.

---
+++ Informatica 5vwo project/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Informatica_5vwo_project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
