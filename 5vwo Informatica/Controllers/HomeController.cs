3a3191c0ef7b01ba80af69a6e0f6007e5e193d2e
Bart van der Poel <bartelomeus.vd.poel@gmail.com>
2021-03-12 12:32:29 +0100

    Routing gedaan voor de 404/500 error page. Nog missende ctrl punt

--- 5vwo Informatica/Controllers/HomeController.cs
+++ 5vwo Informatica/Controllers/HomeController.cs
@@ -27,13 +27,69 @@ namespace _5vwo_Informatica.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }   
+
+        public IActionResult Contact()
+        {
+
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+           app.UseHsts();
+        }   
+
+        [Route("error")]
+                public class ErrorController : Controller
+                {
+                    private readonly TelemetryClient _telemetryClient;
+
+                    public ErrorController(TelemetryClient telemetryClient)
+                    {
+                        _telemetryClient = telemetryClient;
+                    }
+
+                    [Route("500")]
+                    public IActionResult AppError()
+                    {
+                        var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+                        _telemetryClient.TrackException(exceptionHandlerPathFeature.Error);
+                        _telemetryClient.TrackEvent("Error.ServerError", new Dictionary<string, string>
+                        {
+                            ["originalPath"] = exceptionHandlerPathFeature.Path,
+                            ["error"] = exceptionHandlerPathFeature.Error.Message
+                        });
+                        return View();
+                    }
+
+                    [Route("404")]
+                    public IActionResult PageNotFound()
+                    {
+                        string originalPath = "unknown";
+                        if (HttpContext.Items.ContainsKey("originalPath"))
+                        {
+                            originalPath = HttpContext.Items["originalPath"] as string;
+                        }
+                        _telemetryClient.TrackEvent("Error.PageNotFound", new Dictionary<string, string>
+                        {
+                            ["originalPath"] = originalPath
+                        });
+                        return View();
+                    }
+                }
+        
     }
 }
 
