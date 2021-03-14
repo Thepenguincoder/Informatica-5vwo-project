ee5f30469a312f6f497dd4433b3be65448cbf00a
110394 <110394@st-maartenscollege.nl>
2021-03-14 16:00:17 +0100

    Banano image nu toegevoegd

--- Informatica 5vwo project/Controllers/HomeController.cs
+++ Informatica 5vwo project/Controllers/HomeController.cs
@@ -194,50 +194,50 @@ namespace Informatica_5vwo_project.Controllers
             return View(person);
         }
 
-         
+
 
 
 
 
 
         [Route("error")]
-                public class ErrorController : Controller
+        public class ErrorController : Controller
+        {
+            private readonly TelemetryClient _telemetryClient;
+
+            public ErrorController(TelemetryClient telemetryClient)
+            {
+                _telemetryClient = telemetryClient;
+            }
+
+            [Route("500")]
+            public IActionResult AppError()
+            {
+                var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+                _telemetryClient.TrackException(exceptionHandlerPathFeature.Error);
+                _telemetryClient.TrackEvent("Error.ServerError", new Dictionary<string, string>
                 {
-                    private readonly TelemetryClient _telemetryClient;
+                    ["originalPath"] = exceptionHandlerPathFeature.Path,
+                    ["error"] = exceptionHandlerPathFeature.Error.Message
+                });
+                return View();
+            }
 
-                    public ErrorController(TelemetryClient telemetryClient)
-                    {
-                        _telemetryClient = telemetryClient;
-                    }
-
-                    [Route("500")]
-                    public IActionResult AppError()
-                    {
-                        var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
-                        _telemetryClient.TrackException(exceptionHandlerPathFeature.Error);
-                        _telemetryClient.TrackEvent("Error.ServerError", new Dictionary<string, string>
-                        {
-                            ["originalPath"] = exceptionHandlerPathFeature.Path,
-                            ["error"] = exceptionHandlerPathFeature.Error.Message
-                        });
-                        return View();
-                    }
-
-                    [Route("404")]
-                    public IActionResult PageNotFound()
-                    {
-                        string originalPath = "unknown";
-                        if (HttpContext.Items.ContainsKey("originalPath"))
-                        {
-                            originalPath = HttpContext.Items["originalPath"] as string;
-                        }
-                        _telemetryClient.TrackEvent("Error.PageNotFound", new Dictionary<string, string>
-                        {
-                            ["originalPath"] = originalPath
-                        });
-                        return View();
-                    }
+            [Route("404")]
+            public IActionResult PageNotFound()
+            {
+                string originalPath = "unknown";
+                if (HttpContext.Items.ContainsKey("originalPath"))
+                {
+                    originalPath = HttpContext.Items["originalPath"] as string;
                 }
+                _telemetryClient.TrackEvent("Error.PageNotFound", new Dictionary<string, string>
+                {
+                    ["originalPath"] = originalPath
+                });
+                return View();
+            }
+        }
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
