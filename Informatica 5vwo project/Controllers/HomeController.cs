d8964269199e55f09ab51cbcf802caea0a405993
Bart van der Poel <bartelomeus.vd.poel@gmail.com>
2021-06-04 13:05:53 +0200

    404 error pagina en routing gedaan, aka 404 werkt

--- Informatica 5vwo project/Controllers/HomeController.cs
+++ Informatica 5vwo project/Controllers/HomeController.cs
@@ -357,49 +357,12 @@ namespace Informatica_5vwo_project.Controllers
             return View();
         }
 
-        
+         
 
 
 
 
-        [Route("error")]
-        public class ErrorController : Controller
-        {
-            private readonly TelemetryClient _telemetryClient;
-
-            public ErrorController(TelemetryClient telemetryClient)
-            {
-                _telemetryClient = telemetryClient;
-            }
-
-            [Route("500")]
-            public IActionResult AppError()
-            {
-                var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
-                _telemetryClient.TrackException(exceptionHandlerPathFeature.Error);
-                _telemetryClient.TrackEvent("Error.ServerError", new Dictionary<string, string>
-                {
-                    ["originalPath"] = exceptionHandlerPathFeature.Path,
-                    ["error"] = exceptionHandlerPathFeature.Error.Message
-                });
-                return View();
-            }
-
-            [Route("404")]
-            public IActionResult PageNotFound()
-            {
-                string originalPath = "unknown";
-                if (HttpContext.Items.ContainsKey("originalPath"))
-                {
-                    originalPath = HttpContext.Items["originalPath"] as string;
-                }
-                _telemetryClient.TrackEvent("Error.PageNotFound", new Dictionary<string, string>
-                {
-                    ["originalPath"] = originalPath
-                });
-                return View();
-            }
-        }
+      
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -409,17 +372,5 @@ namespace Informatica_5vwo_project.Controllers
         }
     }
 
-    public class TelemetryClient
-    {
-        internal void TrackEvent(string v, Dictionary<string, string> dictionaries)
-        {
-            throw new NotImplementedException();
-        }
-
-        internal void TrackException(Exception error)
-        {
-            throw new NotImplementedException();
-        }
-    }
 }
 
--- Informatica 5vwo project/Startup.cs
+++ Informatica 5vwo project/Startup.cs
@@ -49,9 +49,19 @@ namespace Informatica_5vwo_project
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
+            app.Use(async (context, next) =>
+            {
+                await next();
+                if (context.Response.StatusCode == 404)
+                {
+                    context.Request.Path = "/Error";
+                    await next();
+                }
+
+
+            });
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
